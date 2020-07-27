b2708dbe7109795b8c0a22c07843938b24687224
Farenyk Maximko <maximfarenik@gmail.com>
2020-07-27 18:51:33 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Mother Russia do not suffer");
+        System.out.print("Mother Russia do not suffer");
+    }
+}
